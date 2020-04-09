3af988671d413dae33dd8b888403da648a1cca1b
SINHCOMS LLP <55934694+Sinhcoms-LLP@users.noreply.github.com>
2020-04-09 23:51:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import * as Icon from 'react-feather';
+
+import './App.scss';
+
+import Home from './components/home';
+import Navbar from './components/navbar';
+import Links from './components/links';
+import FAQ from './components/faq';
+import Banner from './components/banner';
+import PatientDB from './components/patientdb';
+import DeepDive from './components/deepdive';
+
+const history = require('history').createBrowserHistory;
+
+function App() {
+  const pages = [
+    {
+      pageLink: '/',
+      view: Home,
+      displayName: 'Home',
+      animationDelayForNavbar: 0.2,
+    },
+    {
+      pageLink: 'https://covid.sinhcoms.com',
+      view: PatientDB,
+      displayName: 'Other Countries',
+      animationDelayForNavbar: 0.3,
+    },
+    {
+      pageLink: '/deepdive',
+      view: DeepDive,
+      displayName: 'Deep Dive',
+      animationDelayForNavbar: 0.4,
+    },
+    {
+      pageLink: '/links',
+      view: Links,
+      displayName: 'Helpful Links',
+      animationDelayForNavbar: 0.4,
+    },
+    {
+      pageLink: '/faq',
+      view: FAQ,
+      displayName: 'About',
+      animationDelayForNavbar: 0.5,
+    },
+  ];
+
+  return (
+    <div className="App">
+      <Router history={history}>
+        <Route
+          render={({location}) => (
+            <div className="Almighty-Router">
+              <Navbar pages={pages} />
+              <Banner />
+              <Route exact path="/" render={() => <Redirect to="/" />} />
+              <Switch location={location}>
+                {pages.map((page, i) => {
+                  return (
+                    <Route
+                      exact
+                      path={page.pageLink}
+                      component={page.view}
+                      key={i}
+                    />
+                  );
+                })}
+                <Redirect to="/" />
+              </Switch>
+            </div>
+          )}
+        />
+      </Router>
+
+      <footer className="fadeInUp" style={{animationDelay: '2s'}}>
+        {<a href="https://www.sinhcoms.com">
+		<img
+          src="https://support.sinhcoms.site/storage/3/sinhcoms.k.png"
+          alt="https://www.sinhcoms.com | Coronavirus cases live dashboard"
+        /></a>}
+         
+        <h5>&copy; 2018 - 2020 Sinhcoms LLP. All right Reserved</h5><h5> We stand with everyone fighting on the frontlines</h5>
+       
+      </footer>
+    </div>
+  );
+}
+
+export default App;
